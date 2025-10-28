f84c34904fc48f766ec480eb8bb37d75e0e245c8
Nin82 <126779011+Nin82@users.noreply.github.com>
2025-10-28 12:02:48 +0100

    Update and rename service-worker.json to service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+// service-worker.js
+const CACHE_NAME = "magazzino-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  console.log("✅ Service Worker installato e cache creata");
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
